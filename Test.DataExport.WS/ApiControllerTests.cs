493c382cb49364bc6288c1ca177429c40e4ad097
Shawn South <shawn.south@bellevuecollege.edu>
2012-11-02 12:15:34 -0700

    Initial commit.

---
+++ Test.DataExport.WS/ApiControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using DataExport.WS.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
+using System.Web.Mvc;
+
+namespace Test.DataExport.WS
+{
+
+
+	[TestClass()]
+	public class ApiControllerTests
+	{
+
+
+		private TestContext testContextInstance;
+
+		public TestContext TestContext
+		{
+			get
+			{
+				return testContextInstance;
+			}
+			set
+			{
+				testContextInstance = value;
+			}
+		}
+
+		#region Additional test attributes
+		// 
+		//You can use the following additional attributes as you write your tests:
+		//
+		//Use ClassInitialize to run code before running the first test in the class
+		//[ClassInitialize()]
+		//public static void MyClassInitialize(TestContext testContext)
+		//{
+		//}
+		//
+		//Use ClassCleanup to run code after all tests in a class have run
+		//[ClassCleanup()]
+		//public static void MyClassCleanup()
+		//{
+		//}
+		//
+		//Use TestInitialize to run code before running each test
+		//[TestInitialize()]
+		//public void MyTestInitialize()
+		//{
+		//}
+		//
+		//Use TestCleanup to run code after each test has run
+		//[TestCleanup()]
+		//public void MyTestCleanup()
+		//{
+		//}
+		//
+		#endregion
+
+		[TestMethod()]
+		public void TestIndex()
+		{
+			ApiController target = new ApiController();
+			ViewResult actual = target.Index() as ViewResult;
+			Assert.IsNotNull(actual);
+		}
+	}
+}
