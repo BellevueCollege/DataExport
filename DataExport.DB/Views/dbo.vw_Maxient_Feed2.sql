8a46d8bbba1caf8962b0719f801a8c64741e66a5
Shawn South <shawn.south@bellevuecollege.edu>
2012-11-28 11:28:21 -0800

    Added view for retrieving data for Maxient Feed 2 - Schedule Data.

---
+++ DataExport.DB/Views/dbo.vw_Maxient_Feed2.sql
@@ -0,0 +1,24 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+/* Schedule Data */
+CREATE VIEW [dbo].[vw_Maxient_Feed2]
+AS
+SELECT
+	e.[SID]
+	,c.CourseID
+	,c.Section
+	,ISNULL(i.Room, '') AS Room
+	,CASE ISNULL(f.AliasName, '')
+		WHEN '' THEN ISNULL(f.FullName, '(Staff)')
+		ELSE f.AliasName
+	 END AS InstructorName
+FROM ODS.dbo.vw_Enrollment e
+INNER JOIN ODS.dbo.vw_Class c ON c.ClassID = e.ClassID
+INNER JOIN ODS.dbo.vw_Instruction i ON i.ClassID = c.ClassID
+LEFT OUTER JOIN ODS.dbo.vw_Employee f ON f.[SID] = i.InstructorSID
+/* TODO: Dynamically identify "current" quarter */
+WHERE e.YearQuarterID = 'B232'
+--ORDER BY e.[SID]
+GO
