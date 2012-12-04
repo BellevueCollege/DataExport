c276f950fd5531c9322e8d5f85cc8537428542b9
Shawn South <shawn.south@bellevuecollege.edu>
2012-12-03 16:53:34 -0800

    Added version to index page.

--- DataExport.WS/Properties/AssemblyInfo.cs
+++ DataExport.WS/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bellevue College")]
 [assembly: AssemblyProduct("DataExport.WebService")]
-[assembly: AssemblyCopyright("Copyright ©  2012 Bellevue College")]
+[assembly: AssemblyCopyright("Copyright © 2012 Bellevue College")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("0.9.0.1")]
+[assembly: AssemblyFileVersion("0.9.0.1")]
