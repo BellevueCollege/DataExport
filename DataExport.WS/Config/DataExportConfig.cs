d8f6e27d8079f4de951dff7393d84e05da7a85d8
Shawn South <shawn.south@bellevuecollege.edu>
2012-12-12 12:17:34 -0800

    Moved config classes into their own folder.

---
+++ DataExport.WS/Config/DataExportConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataExport.WS.Controllers;
+
+namespace DataExport.Web
+{
+	public class DataExportConfig
+	{
+		public ICollection<IExporter> Exporters {get;set;}
+	}
+}
