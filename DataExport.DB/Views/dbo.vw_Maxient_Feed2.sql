a19c5060f18cf5fd8e09567c3fba745b472d274f
Shawn South <shawn.south@bellevuecollege.edu>
2013-01-04 17:43:23 -0800

    Removed TODO note.

--- DataExport.DB/Views/dbo.vw_Maxient_Feed2.sql
+++ DataExport.DB/Views/dbo.vw_Maxient_Feed2.sql
@@ -23,7 +23,7 @@ FROM ODS.dbo.vw_Enrollment e
 INNER JOIN ODS.dbo.vw_Class c ON c.ClassID = e.ClassID
 INNER JOIN ODS.dbo.vw_Instruction i ON i.ClassID = c.ClassID
 LEFT OUTER JOIN ODS.dbo.vw_Employee f ON f.[SID] = i.InstructorSID
-/* TODO: Dynamically identify "current" quarter */
+/* Dynamically identify "current" quarter */
 WHERE e.YearQuarterID IN (
 		/* Most recently completed quarter */
 		SELECT TOP 1 y.YearQuarterID FROM ODS.dbo.vw_YearQuarter y
