2b796c9bd5402c46ea71a212642df95d5811f63e
Shawn South <shawn.south@bellevuecollege.edu>
2013-08-27 13:49:42 -0700

    Updated w/ permission changes made in DB

--- DataExport.DB/Security/Roles/db_datareader.sql
+++ DataExport.DB/Security/Roles/db_datareader.sql
@@ -1,4 +1,8 @@
 
+EXEC sp_addrolemember N'db_datareader', N'CAMPUS\N216-E027543$'
+
+EXEC sp_addrolemember N'db_datareader', N'svc-datareader-dev'
+
 EXEC sp_addrolemember N'db_datareader', N'CAMPUS\SSOUTHDT-N216K$'
 
 GO
--- DataExport.DB/Security/Roles/db_datawriter.sql
+++ DataExport.DB/Security/Roles/db_datawriter.sql
@@ -1,2 +1,4 @@
+
+EXEC sp_addrolemember N'db_datawriter', N'CAMPUS\N216-E027543$'
 EXEC sp_addrolemember N'db_datawriter', N'CAMPUS\SSOUTHDT-N216K$'
 GO
